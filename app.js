9bff99ac1c9c680da2593666b87f48dff953f0cf
NavyaPasunuri <navyapasunuri06@gmail.com>
2026-03-26 11:13:47 +0530

    intial commit

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const app = express();
 // Root route
 app.get('/', (req, res) => {
   const sum = add(2, 3);
-  res.send(`Sum is: ${sum}`);
+ // res.send(`Sum is: ${sum}`);
+ res.send('navya is good girl');
 });
 
 // App listens on port 8080
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
+
+
+
